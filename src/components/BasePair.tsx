fbaa6ff8d79b3637efc3d8f940d08f3c863deac9
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-12 17:50:05 -0700

    add onclick to get trading pairs for base pair

--- src/components/BasePair.tsx
+++ src/components/BasePair.tsx
@@ -3,16 +3,18 @@ import '../css/base-pair.css';
 
 interface BasePairProps {
   id: string;
+  exchange_id: string;
   symbol: string;
   updated_at: string;
+  fetchTradingPairs: (exchange_id: string, basePairId: string) => void;
 }
 
 function BasePair(props: BasePairProps) {
-  // console.log(props)
-  // const [basePairsList, setBasePairsList] = useState<BasePairUI[]>([])
   return (
     <div>
-      <button className="alt-btn">{props.symbol}</button>
+      <button className="alt-btn"
+        onClick={() => props.fetchTradingPairs(props.exchange_id, props.id)}
+      >{props.symbol}</button>
     </div>
   )
 }
