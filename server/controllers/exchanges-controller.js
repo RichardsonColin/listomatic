0573133d0d449a401ac84c7c6a81007e8823d802
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-12 17:48:15 -0700

    add route to get an exchange's trading pairs

--- server/controllers/exchanges-controller.js
+++ server/controllers/exchanges-controller.js
@@ -25,3 +25,19 @@ exports.exchangesGetBasePairs = async (req, res) => {
     ORDER BY bp.symbol;`, [id])
   res.json(rows)
 }
+
+exports.exchangesGetTradingPairs = async (req, res) => {
+  const { exchange_id, base_pair_id } = req.params
+  const { rows } = await db.query(`SELECT DISTINCT c.id, e.name AS exchange_name, bp.symbol AS base_pair_symbol, c.symbol AS cryptocurrency_symbol
+    FROM exchange_cryptocurrency_base_pair_references ref
+    INNER JOIN exchanges e
+    ON e.id = ref.exchange_id
+    INNER JOIN base_pairs bp
+    ON bp.id = ref.base_pair_id
+    INNER JOIN cryptocurrencies c
+    ON c.id = ref.cryptocurrency_id
+    WHERE ref.exchange_id = $1
+    AND ref.base_pair_id = $2
+    ORDER BY bp.symbol;`, [exchange_id, base_pair_id])
+  res.json(rows)
+}
