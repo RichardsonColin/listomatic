8a5b27be9a2c6a8c49e81adae94d9f25da311d9c
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-20 08:42:59 -0700

    change pg init for heroku integration

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-})
+const db_config = process.env.NODE_ENV && process.env.NODE_ENV !== 'development' ?
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  } :
+  {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  }
+
+const pool = new Pool(db_config)
 
 module.exports = {
   query: async (text, params) => {
